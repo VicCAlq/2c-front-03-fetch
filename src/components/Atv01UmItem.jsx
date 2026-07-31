27fd9c9608674f55d1f0e6c1753b6f63da22eb82
Mylle <kamylle.vitoria@nave.org.br>
2026-07-31 11:10:15 -0300

    Update Atv01UmItem.jsx

--- src/components/Atv01UmItem.jsx
+++ src/components/Atv01UmItem.jsx
@@ -22,3 +22,73 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
+
+
+import { useState } from "react";
+<p>
+  {Atv01UmItem.id} - Minha atividade:{" "}
+  {Atv01UmItem.completed ? "feito" : "a fazer"}
+</p>
+
+const estilo = {
+  atividade: {
+    backgroundColor: "#dac",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "20px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+    border: "none",
+    cursor: "pointer",
+  },
+  resultado: {
+    margin: "10px",
+    padding: "5px",
+    backgroundColor: "#a89",
+    borderRadius: "5px",
+  },
+};
+
+export default function Atv01UmItem() {
+  const [atividade, setAtividade] = useState(null);
+
+  async function carregarAtividade() {
+    const resposta = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+
+    const dados = await resposta.json();
+    dados.completed = false;
+    setAtividade(dados);
+  }
+
+  return (
+    <div style={estilo.atividade}>
+      <p>Carregue a atividade abaixo:</p>
+
+      <button style={estilo.botao} onClick={carregarAtividade}>
+        <p style={estilo.textoBotao}>
+          Clique abaixo para carregar uma atividade
+        </p>
+      </button>
+
+     {atividade && (
+      <div style={estilo.resultado}>
+        <p>
+          {atividade.id} - {atividade.title}:{" "}
+          {atividade.completed ? "feito" : "a fazer"}
+        </p>
+      </div>
+
+    )}
+    </div>
+  );
+}
