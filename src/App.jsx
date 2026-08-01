125ce36dd636a31151f84daba4748853772bff4e
marcus-vinicios-nave <marcus.vinicios@nave.org.br>
2026-08-01 00:57:55 +0000

    primeira feita

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Exemplo01Fetch from "./components/Exemplo01FetchSemComentarios";
 import Exemplo02Tratamento from "./components/Exemplo02TratamentoSemComentarios";
 import Exemplo03VariosItens from "./components/Exemplo03VariosItensSemComentarios";
+import Atv01UmItem from "./components/Atv01UmItem.jsx"
+
 
 const styles = {
   container: {
@@ -20,6 +22,7 @@ export default function App() {
       <Exemplo01Fetch/>
       <Exemplo02Tratamento/>
       <Exemplo03VariosItens/>
+      <Atv01UmItem></Atv01UmItem>
     </div>
   );
 }
