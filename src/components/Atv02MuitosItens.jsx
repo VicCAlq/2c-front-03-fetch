46ac32e4d30bdd024dbd2391b6f5c0ca03807c11
Andrey Vieira <andreyantonio2020@gmail.com>
2026-08-02 00:41:30 -0300

    2

--- src/components/Atv02MuitosItens.jsx
+++ src/components/Atv02MuitosItens.jsx
@@ -23,3 +23,31 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
+import { useState } from "react";
+
+export default function Atv02MuitosItens() {
+  const [atividades, setAtividades] = useState([]);
+
+  const carregarAtividades = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => setAtividades(data));
+  };
+
+  return (
+    <div>
+      <button onClick={carregarAtividades}>
+        Clique abaixo para carregar várias atividades
+      </button>
+
+      <div>
+        {atividades.map((atividade) => (
+          <p key={atividade.id}>
+            {atividade.id} - {atividade.title}:{" "}
+            {atividade.completed ? "feito" : "a fazer"}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+}
