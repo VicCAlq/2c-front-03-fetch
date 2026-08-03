51d18711fa9c3f0cc01bb99437b8a6b8dff94942
Arthur Emanuel <arthur.emanuel@nave.org.br>
2026-08-03 11:50:29 -0300

    Update Atv04TratarErrosDeMuitos.jsx

--- src/components/Atv04TratarErrosDeMuitos.jsx
+++ src/components/Atv04TratarErrosDeMuitos.jsx
@@ -28,3 +28,43 @@
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
   */
+
+export default function Atv04TratarErrosDeMuitos() {
+  const [itens, setItens] = useState(<p>
+    Os comentários aparecerão aqui
+  </p>)
+
+  async function carregarComentarios() {
+  
+    await fetch(
+      "https://jsonplaceholder.typicode.com/comments",     { method: 'GET', }
+    )
+    .then((resposta) => {
+      if(!resposta.ok == true) {
+        throw new Error (`Erro na requisição! Status:" ${resposta.status}`);
+      }
+      return resposta.json()
+    })
+    .then((resultado) => {
+      setItens(<div style={{ display: "flex", flexDirection: "column", gap: "10px"}}>
+
+        {resultado.map((comentario) => {
+          return <div style={{backgroundColor: "#aabbff", padding: "10px", width: "500px", margin: "10px auto"}}>
+            <p>{comentario.postId}: {comentario.id} - {comentario.email}</p>
+            <p>{comentario.name}</p>
+            <p>{comentario.body}</p>
+            </div>;
+        })}
+      </div>)
+    })
+    .catch((erro) => {
+      window.alert(erro)
+    })
+  }
+  
+  
+  return(<div style={estilo.atividade}>
+    <button style={estilo.botao}  onClick = {() => carregarComentarios()}>Clique abaixo para carregar uma atividade</button>
+    {itens}
+  </div>)
+}
