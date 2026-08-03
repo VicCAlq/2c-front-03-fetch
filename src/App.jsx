aadbded984a519d796490bb212dc9483834eaf67
zivshw <ziv.tabatchnik@nave.org.br>
2026-08-03 10:53:44 -0300

    Atividade Atv01UmItem concluída.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import Exemplo01Fetch from "./components/Exemplo01FetchSemComentarios";
-import Exemplo02Tratamento from "./components/Exemplo02TratamentoSemComentarios";
-import Exemplo03VariosItens from "./components/Exemplo03VariosItensSemComentarios";
+import Atv01UmItem from "./components/Atv01UmItem";
+import Atv02MuitosItens from "./components/Atv02MuitosItens";
+import Atv03TratarErrosDeUm from "./components/Atv03TratarErrosDeUm";
+import Atv04TratarErrosDeMuitos from "./components/Atv04TratarErrosDeMuitos";
 
 const styles = {
-  container: {
-    flex: 1,
-    backgroundColor: "#eec",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: "#101015"
-  }
+	container: {
+		flex: 1,
+		backgroundColor: "#eec",
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		color: "#101015"
+	}
 };
 
 export default function App() {
-  return (
-    <div style={styles.container}>
-      <Exemplo01Fetch/>
-      <Exemplo02Tratamento/>
-      <Exemplo03VariosItens/>
-    </div>
-  );
+	return (
+		<div style={styles.container}>
+			<h1>Atividades para N1</h1>
+
+			<h2>Atividade 01</h2>
+			<Atv01UmItem />
+
+			<h2>Atividade 02</h2>
+			<Atv02MuitosItens />
+
+			<h2>Atividade 03</h2>
+			<Atv03TratarErrosDeUm />
+
+			<h2>Atividade 04</h2>
+			<Atv04TratarErrosDeMuitos />
+		</div>
+	);
 }
