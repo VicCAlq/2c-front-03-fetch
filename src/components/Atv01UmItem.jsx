a75829fb2207775c843b383dfa0ae5e613193d69
amanda-vitoria-nave <amanda.rodrigues@nave.org.br>
2026-08-03 11:50:03 -0300

    Update Atv01UmItem.jsx

--- src/components/Atv01UmItem.jsx
+++ src/components/Atv01UmItem.jsx
@@ -22,3 +22,43 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
+
+import { useState } from 'react'
+
+export default function Atv01UmItem() {
+
+  const [resultado, setResultado] = useState(<p>Os dados do usuário aparecerão no lugar deste texto</p>)
+
+  async function carregarUsuario() {
+    await fetch(
+      'https://jsonplaceholder.typicode.com/todos/1',
+      { method: 'GET', }
+    )
+    .then((resposta) => {
+      console.log(resposta)
+      return resposta.json()
+    })    
+    .then((resultado) => {
+      console.log(resultado)
+      const usuario = <div style={{
+        margin: "10px", padding: "5px", backgroundColor: "#a89", borderRadius: "5px",
+      }}>
+       [comentarios.id] - [comentarios.title]: [comentarios.status]
+      </div>
+
+      setResultado(usuario)
+    })
+  }
+
+  return(
+    <div style={estilo.usuario}>
+      <p>
+        Carregue o usuário abaixo:
+      </p>
+      <button style={estilo.botao} onClick={() => carregarUsuario()}>
+        <p style={estilo.textoBotao}>Carregar usuário</p>
+      </button>
+      {resultado}
+    </div>
+  )
+}
