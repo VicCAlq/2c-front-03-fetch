ace48f37cdf06e7e5b6349e25f8e40cd6a6a6152
arthur-lucio-nave <arthur.lucio@nave.org.br>
2026-08-04 09:21:43 -0300

    enviado

--- src/components/Atv01UmItem.jsx
+++ src/components/Atv01UmItem.jsx
@@ -22,3 +22,33 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
+import { useState } from "react";
+
+function Atv01UmItem() {
+  const [atividade, setAtividade] = useState(null);
+
+  const carregarAtividade = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setAtividade(dados);
+      });
+  };
+
+  return (
+    <div>
+      <button onClick={carregarAtividade}>
+        Clique abaixo para carregar uma atividade
+      </button>
+
+      {atividade && (
+        <p>
+          {atividade.id} - {atividade.title}:{" "}
+          {atividade.completed ? "feito" : "a fazer"}
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default Atv01UmItem;
--- src/components/Atv02MuitosItens.jsx
+++ src/components/Atv02MuitosItens.jsx
@@ -23,3 +23,35 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
+import { useState } from "react";
+
+function Atv02MuitosItens() {
+  const [atividades, setAtividades] = useState([]);
+
+  const carregarAtividades = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setAtividades(dados);
+      });
+  };
+
+  return (
+    <div>
+      <button onClick={carregarAtividades}>
+        Clique abaixo para carregar várias atividades
+      </button>
+
+      <div>
+        {atividades.map((atividade) => (
+          <p key={atividade.id}>
+            {atividade.id} - {atividade.title}:{" "}
+            {atividade.completed ? "feito" : "a fazer"}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Atv02MuitosItens;
